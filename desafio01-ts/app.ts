5fd5a20ff182855270eb4d6a0a1ec1f5176aa94a
AndreyWBS <andrey.wesley.sa@gmail.com>
2025-01-23 15:31:32 -0300

    primeiro commit

---
+++ desafio01-ts/app.ts
@@ -0,0 +1,10 @@
+import { CompanyAccount } from './class/CompanyAccount'
+import { PeopleAccount } from './class/PeopleAccount'
+
+const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Nath', 10)
+console.log(peopleAccount)
+peopleAccount.deposit()
+const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
+companyAccount.deposit()
+console.log(companyAccount)
+console.log("mano isso é um teste")
